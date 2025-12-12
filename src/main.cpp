0348d66804787c2291a87cce360ca7a3f0bb7568
soethiha <usersoe123@gmail.com>
2025-12-12 17:54:47 +0630

    hr

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "raylib.h"
 #include <assert.h>
+#include <complex>
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <complex>
-#include <math.h>
+#include "plug.h"
 // #if defined(PLATFORM_WEB)
 //     #include <emscripten/emscripten.h>
 // #endif
@@ -20,16 +21,16 @@
 #define SINGLE_SAMPLE_MIN INT16_MIN
 
 typedef struct {
-    float left;
-    float right;
-}Frame;
+  float left;
+  float right;
+} Frame;
 #define N 256
 float in[N] = {0};
 std::complex<float> out[N] = {0};
 float max_amp = 0;
 static const int screenWidth = 800;
 static const int screenHeight = 450;
-Frame global_frames[4800*2] = {0};
+Frame global_frames[4800 * 2] = {0};
 size_t global_frames_count = 0;
 
 //----------------------------------------------------------------------------------
@@ -56,7 +57,8 @@ void fft(float in[], size_t stride, std::complex<float> out[], size_t n) {
 float amp(std::complex<float> z) {
   float a = fabsf(std::real<float>(z));
   float b = fabsf(std::imag<float>(z));
-  if (a < b) return b;
+  if (a < b)
+    return b;
   return a;
 }
 
@@ -65,7 +67,7 @@ void callback(void *bufferData, unsigned int frames) {
     return;
   }
 
-  Frame *fframes = (Frame*)bufferData;
+  Frame *fframes = (Frame *)bufferData;
 
   for (size_t i = 0; i < frames; i++) {
     in[i] = fframes[i].left;
@@ -78,21 +80,40 @@ void callback(void *bufferData, unsigned int frames) {
       max_amp = a;
     }
   }
-
 }
 
+char *shit_args(int *argc, char ***argv) {
+  assert(*argc > 0);
+  char *result = (**argv);
+  (*argv) += 1;
+  (*argc) -= 1;
+  return result;
+}
 
-    //----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
 // Program main entry point
 //----------------------------------------------------------------------------------
-int main(void) {
+int main(int argc, char **argv) {
   // Initialization
   //---------------------------------------------------------
+  plug_hello();
+  return 0;
+
+  const char *program = shit_args(&argc, &argv);
+  if (argc == 0) {
+    fprintf(stderr, "Usage: %s <input>\n", program);
+    fprintf(stderr, "ERROR: no input file is provided\n");
+    return 1;
+  }
+
+  const char *file_path = shit_args(&argc, &argv);
+
+  SetWindowState(FLAG_WINDOW_ALWAYS_RUN);
   InitWindow(screenWidth, screenHeight, "raylib game template");
   SetTargetFPS(60);
 
   InitAudioDevice();
-  Music music = LoadMusicStream("ai_kotoba4.mp3");
+  Music music = LoadMusicStream(file_path);
 
   assert(music.stream.sampleSize == 32);
   assert(music.stream.channels == 2);
@@ -126,24 +147,26 @@ int main(void) {
     ClearBackground(RAYWHITE);
     float cell_width = (float)screenWidth / N;
     for (size_t i = 0; i < N; i++) {
-      float t = amp(out[i])/ max_amp;
-      DrawRectangle(i * cell_width, screenHeight / 2 - screenHeight / 2 * t, cell_width,
-                    screenHeight / 2 * t, RED);
+      float t = amp(out[i]) / max_amp;
+      DrawRectangle(i * cell_width, screenHeight / 2 - screenHeight / 2 * t,
+                    cell_width, screenHeight / 2 * t, RED);
     }
 
-    //for (size_t i = 0; i < global_frames_count; i++) {
-    //    float t = global_frames[i].left;
-    //  if (t > 0) {
-    //    /*float t = (float)sample / SINGLE_SAMPLE_MAX;*/
-    //    DrawRectangle(i * cell_width, screenHeight / 2 - screenHeight / 2 * t,
-    //                  1, screenHeight / 2 * t, RED);
-    //  } else {
-    //    /*float t = (float)sample / SINGLE_SAMPLE_MIN; */
-    //    DrawRectangle(i * cell_width, screenHeight/2, 1, screenHeight / 2 * t, RED);
-    //  }
-    //}
-    // DrawText("Congrats! You created your first window!", 190, 200, 20,
-    // LIGHTGRAY); if(global_frames_count > 0) exit(1);
+    // for (size_t i = 0; i < global_frames_count; i++) {
+    //     float t = global_frames[i].left;
+    //   if (t > 0) {
+    //     /*float t = (float)sample / SINGLE_SAMPLE_MAX;*/
+    //     DrawRectangle(i * cell_width, screenHeight / 2 - screenHeight / 2 *
+    //     t,
+    //                   1, screenHeight / 2 * t, RED);
+    //   } else {
+    //     /*float t = (float)sample / SINGLE_SAMPLE_MIN; */
+    //     DrawRectangle(i * cell_width, screenHeight/2, 1, screenHeight / 2 *
+    //     t, RED);
+    //   }
+    // }
+    //  DrawText("Congrats! You created your first window!", 190, 200, 20,
+    //  LIGHTGRAY); if(global_frames_count > 0) exit(1);
     EndDrawing();
   }
   // #endif
